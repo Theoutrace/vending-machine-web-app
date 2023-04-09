e6bb50f247ae016ece7aacf94fc1a56c77701ed8
Theoutrace <prakashkumar.imw@gmail.com>
2023-04-09 23:20:15 +0530

    transaction panel improved

--- client/src/components/transactions/SingleTransaction.jsx
+++ client/src/components/transactions/SingleTransaction.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+/**
+{
+    _id: "a125435asd4590",
+    status: "Success",
+    machineId: "m2",
+    employeeId: "E3",
+    slotname: "Donut",
+}
+*/
+
+import React, { useState } from "react";
 import "./SingleTransaction.css";
+import { Box } from "@mui/material";
+import removeIcon from "./images/remove.png";
 
 const SingleTransaction = (props) => {
-  return <div className="SingleTransaction-outer-dv">{props.item.status}</div>;
+  const [showRemove, setShowRemove] = useState(false);
+  const obj = props.item;
+  const transactionRemovehandler = () => {
+    console.log("Removed Transaction obj: ", props.item);
+  };
+  return (
+    <div
+      className={`SingleTransaction-outer-dv ${
+        obj.status ? "success-cls" : "failed-cls"
+      } ${props.status && "heading-cls"}`}
+      onMouseEnter={() => setShowRemove(true)}
+      onMouseLeave={() => setShowRemove(false)}
+    >
+      <Box sx={{ width: "20%", margin: "5px" }}>{obj._id}</Box>
+
+      {props.status ? (
+        <Box sx={{ width: "10%", margin: "5px" }}>Status</Box>
+      ) : (
+        <Box sx={{ width: "10%", margin: "5px" }}>
+          {obj.status ? "Success" : "Failed"}
+        </Box>
+      )}
+      <Box sx={{ width: "20%", margin: "5px" }}>{obj.employeeId}</Box>
+      <Box sx={{ width: "20%", margin: "5px" }}>{obj.machineId}</Box>
+      <Box sx={{ width: "10%", margin: "5px" }}>{obj.slotname}</Box>
+      {showRemove && !props.status && (
+        <img src={removeIcon} alt="" onClick={transactionRemovehandler} />
+      )}
+    </div>
+  );
 };
 
 export default SingleTransaction;
--- client/src/components/transactions/Transactions.jsx
+++ client/src/components/transactions/Transactions.jsx
@@ -2,18 +2,53 @@ import React from "react";
 import "./Transactions.css";
 import SingleTransaction from "./SingleTransaction";
 const DummyTrasnsactions = [
-  { status: "Success" },
-  { status: "failed" },
-  { status: "Success" },
-  { status: "Success" },
+  {
+    _id: "a125435asd4587",
+    status: true,
+    machineId: "m2",
+    employeeId: "E1",
+    slotname: "Cola",
+  },
+  {
+    _id: "a125435asd4588",
+    status: false,
+    machineId: "m1",
+    employeeId: "E3",
+    slotname: "Chips",
+  },
+  {
+    _id: "a125435asd4589",
+    status: true,
+    machineId: "m3",
+    employeeId: "E2",
+    slotname: "Chips",
+  },
+  {
+    _id: "a125435asd4590",
+    status: true,
+    machineId: "m2",
+    employeeId: "E3",
+    slotname: "Donut",
+  },
 ];
+
+const heading = {
+  _id: "ID",
+  status: "Status",
+  machineId: "Machine ID",
+  employeeId: "Employee ID",
+  slotname: "Slot Name",
+};
 
 const Transactions = () => {
   return (
     <div className="Transactions-outer-cls">
-      {DummyTrasnsactions.map((item) => {
-        return <SingleTransaction item={item} />;
-      })}
+      <SingleTransaction item={heading} status={heading.status} />
+      <>
+        {DummyTrasnsactions.map((item) => {
+          return <SingleTransaction item={item} />;
+        })}
+      </>
     </div>
   );
 };
